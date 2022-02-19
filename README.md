e999ca2ebdd9dc82f2d3102bca64ab9092ed3337
Jdbain67 <james.daniel.bain@gmail.com>
2022-02-18 19:39:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-bbmgpe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-bbmgpe)
---
+++ client/build/precache-manifest.c62c4a599f80960917ed24abaa8a1d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f985d232efe5c8da065e",
+    "url": "/static/css/main.061c7f27.chunk.css"
+  },
+  {
+    "revision": "f985d232efe5c8da065e",
+    "url": "/static/js/main.359e5483.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93f8723be1ce4424aa4b",
+    "url": "/static/js/2.9d28d9c4.chunk.js"
+  },
+  {
+    "revision": "864e409d2ca8c5e978aeb7ce462e6fca",
+    "url": "/index.html"
+  }
+];
